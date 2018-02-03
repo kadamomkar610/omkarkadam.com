2b2965994f51a7965a1cb63f022e0ba5facc2c26
kadamomkar610 <kadam.omkar610@gmail.com>
2018-02-03 17:32:12 +0000

    ip code added

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+  
+
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -23,11 +26,41 @@ Email:      omkark610@gmail.com<br>
 Location:   Dombivli<br>
 <br><br>
 I am Social:<br>
+   
+<a href="https://www.linkedin.com/in/omkark610/" class="fa fa-linkedin cust-fa" target="_blank" style="
+    padding: 20px !important;
+    font-size: 50px !important;
+    width: 50px !important;
+    text-align: center !important;
+    text-decoration: none !important;
+    margin: 5px 2px !important;
+"></a> 
+<a href="https://www.facebook.com/kadam.omkar610" class="fa fa-facebook cust-fa" target="_blank" style="
+    padding: 20px !important;
+    font-size: 50px !important;
+    width: 50px !important;
+    text-align: center !important;
+    text-decoration: none !important;
+    margin: 5px 2px !important;  
+"></a>
+<a href="https://twitter.com/omkark610" class="fa fa-twitter cust-fa" target="_blank" style="
+    padding: 20px !important;
+    font-size: 50px !important;
+    width: 50px !important;
+    text-align: center !important;
+    text-decoration: none !important;
+    margin: 5px 2px !important;
+"></a>
+<a href="https://www.instagram.com/omkark610/" class="fa fa-instagram cust-fa" target="_blank" style="
+    padding: 20px !important;
+    font-size: 50px !important;
+    width: 50px !important;
+    text-align: center !important;
+    text-decoration: none !important;
+    margin: 5px 2px !important;
+"></a>
 
-LinkedIn:   <a href="https://www.facebook.com/kadam.omkar610" target="_blank">https://www.facebook.com/kadam.omkar610</a><br>
-Facebook:   <a href="https://www.linkedin.com/in/omkark610/" target="_blank">https://www.linkedin.com/in/omkark610</a><br>
-Instagram:  <a href="https://www.instagram.com/omkark610/" target="_blank">https://www.instagram.com/omkark610</a><br>
-Twitter:    <a href="https://twitter.com/omkark610" target="_blank">https://twitter.com/omkark610</a><br>
+
  
                     </p>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,13 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/work','HomeController@work')->name('work');
 Route::get('/contact','HomeController@contact')->name('contact');
 
+Route::get('/ip', function () {
+    $ip= \Request::ip();
+    $data = \Location::get($ip);
+    dd($data);
+});
+
+
 
 // Route::get('login/facebook', 'Auth\LoginController@redirectToProvider');
 // Route::get('login/facebook/callback', 'Auth\LoginController@handleProviderCallback');
